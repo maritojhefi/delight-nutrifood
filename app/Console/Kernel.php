5184e6f3ca398e16520060dba0a84ac4ac187a89
Mario <maritojhefi@outlook.com>
2022-02-14 16:58:09 -0400

    schedule por comando 'apertura:diaria' 1.1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('apertura:diaria')
-        ->daily();
+        ->everyMinute();
         // $schedule->command('inspire')->hourly();
         /*$schedule->call(function () {
            
