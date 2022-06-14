4947c13256ed38faf5dce47a7c04b1b27fc6f475
Mario <maritojhefi@outlook.com>
2022-06-14 14:31:18 -0400

    cambio de seeder pa limpiar registros

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -14,64 +14,6 @@ class ProductoSeeder extends Seeder
      */
     public function run()
     {
-        Producto::create(
-            ['nombre'=>'Licuado de agua',
-            'subcategoria_id'=>6,
-            'precio'=>'5',
-            'detalle'=>'nada',
-            'imagen'=>'delight_logo.jpg',
-            'estado'=>'activo',
-            'codigoBarra'=>'delight-123',
-            'descuento'=>'4',
-            'puntos'=>'1',
-            ]);
-            Producto::create(
-                ['nombre'=>'Sandwich integral',
-                'subcategoria_id'=>'2',
-                'precio'=>'12',
-                'detalle'=>'nada',
-                'imagen'=>'delight_logo.jpg',
-                'estado'=>'activo',
-                'codigoBarra'=>'delight-123',
-                'contable'=>false,
-                'puntos'=>'2',
-                ]);
-
-                Producto::create(
-                    ['nombre'=>'Almuerzo ejecutivo',
-                    'subcategoria_id'=>'8',
-                    'precio'=>'15.5',
-                    'detalle'=>'almuerzo que incluye pollo asado, papa hervida etc',
-                    'imagen'=>'delight_logo.jpg',
-                    'estado'=>'activo',
-                    'codigoBarra'=>'delight-123',
-                    'contable'=>true,
-                    'puntos'=>'3',
-                    ]);
-
-                    Producto::create(
-                        ['nombre'=>'Almuerzo vegano',
-                        'subcategoria_id'=>'8',
-                        'precio'=>'20.50',
-                        'detalle'=>'almuerzo con pollo a la plancha, brocoli, pure de almejas',
-                        'imagen'=>'delight_logo.jpg',
-                        'estado'=>'activo',
-                        'codigoBarra'=>'delight-123',
-                        'contable'=>false,
-                        'puntos'=>'4',
-                        ]);
-
-                        Producto::create(
-                            ['nombre'=>'Grano de cafe',
-                            'subcategoria_id'=>'7',
-                            'precio'=>'0.75',
-                            'detalle'=>'nada',
-                            'imagen'=>'delight_logo.jpg',
-                            'estado'=>'activo',
-                            'codigoBarra'=>'delight-123',
-                            'contable'=>true,
-                            'puntos'=>'1',
-                            'medicion'=>'gramo'
-                            ]);
+        Producto::truncate();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        
+        User::truncate();
         User::create(
             ['name'=>'Jacquelinne',
             'password'=>'delight123',
@@ -25,15 +25,6 @@ class UserSeeder extends Seeder
         'password'=>'jhefi123',
         'email'=>'maritojhefi@gmail.com',
          'role_id'=>1]);
-         User::create(
-            ['name'=>'Villa',
-            'password'=>'villa123',
-            'email'=>'villa@gmail.com',
-             'role_id'=>4]);
-             User::create(
-                ['name'=>'Jhefi',
-                'password'=>'jhefi123',
-                'email'=>'jhefi@gmail.com',
-                 'role_id'=>4]);
+        
     }
 }
