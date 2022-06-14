883dfdf21da3b249960320da8eb7ea7e73303acc
Mario <maritojhefi@outlook.com>
2022-06-14 14:39:36 -0400

    cambio de seeder pa limpiar registros 2

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
+        
         User::create(
             ['name'=>'Jacquelinne',
             'password'=>'delight123',
