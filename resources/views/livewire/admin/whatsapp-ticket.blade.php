0f4f85bcabbded8e1cc6254a32fbdd03be494d16
unknown <maritojhefi@gmail.com>
2022-06-22 12:43:48 -0400

    listo

--- resources/views/livewire/admin/whatsapp-ticket.blade.php
+++ resources/views/livewire/admin/whatsapp-ticket.blade.php
@@ -21,8 +21,9 @@
                                 
                             </thead>
                             <tbody>
-                                <tr>
+                               
                                     @foreach ($tickets as $ticket)
+                                    <tr>
                                     <td>{{$ticket->cliente->name}}</td>
                                     <td>{{$ticket->cantidad}}</td>
                                     <td>{{$ticket->paso_segundo?'FINALIZADO':'PENDIENTE'}}</td>
@@ -31,8 +32,9 @@
                                     <td>{{$ticket->paso_metodo_empaque?'FINALIZADO':'PENDIENTE'}}</td>
                                     <td>{{$ticket->created_at->format('d-M-Y')}}</td>
                                     <td><button wire:click="eliminar({{$ticket->id}})" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button></td>
-                                    @endforeach
                                 </tr>
+                                    @endforeach
+                                
                             </tbody>
                            
 
