28ceb294752f469e0cc551fa24a596f0ed92d9c2
Mario <maritojhefi@outlook.com>
2022-07-06 21:54:03 -0400

    listo

--- resources/views/client/miperfil/edit.blade.php
+++ resources/views/client/miperfil/edit.blade.php
@@ -71,7 +71,7 @@
                         </div>
                         <div class="input-style has-borders no-icon mb-4">
                             <input type="date" name="nacimiento" id="form6" value="{{ old('nacimiento', $usuario->nacimiento) }}" max="2010-01-01" min="1950-01-01" class="form-control validate-text">
-                            <label for="form6" class="color-highlight">Fecha Nacimiento</label>
+                            
                             @error('nacimiento')
                                 <i class="fa fa-times  invalid color-red-dark"></i>
                                 <p class="text-danger">{{ $message }}</p>
