fbfd897f5f2a8f6e7b9cb2d8ea5c6f375fdda7f3
unknown <maritojhefi@gmail.com>
2022-07-27 12:33:25 -0400

    listo

--- resources/views/admin/otros/marcado.blade.php
+++ resources/views/admin/otros/marcado.blade.php
@@ -1,41 +1,53 @@
 @extends('client.master')
 @section('content')
     <x-cabecera-pagina titulo="Hola!" cabecera="bordeado" />
-    
+
     <div class="card card-style text-center" id="close">
         <form action="{{ route('registrar.asistencia') }}" method="post" id="myform">
             @csrf
             <input type="hidden" id="latitud" name="latitud">
             <input type="hidden" id="longitud" name="longitud">
-           
         </form>
         <div class="content py-5">
-            
-            <img src="{{asset('cargando.gif')}}" alt="">
+
+            <img src="{{ asset('cargando.gif') }}" alt="">
             <h2 class="">Espera!</h2>
             <br>
             <h5 class="text-uppercase pb-3 mt-3">Registrando la hora!</h5>
             <p class="boxed-text-l">
-                 Delight by Macrobyte
+                Delight by Macrobyte
             </p>
             <div class="row mb-0">
-               
+
             </div>
         </div>
     </div>
     <div class="card card-style text-center d-none" id="tooFar">
-        
+
         <div class="content py-5">
-            
-            <img src="{{asset('map.gif')}}" alt="">
+
+            <img src="{{ asset('map.gif') }}" alt="">
             <h3 class="">Estas lejos!</h3>
             <br>
             <h5 class="text-uppercase pb-3 mt-3">Debes estar mas cerca para registrar!</h5>
             <p class="boxed-text-l" id="textoCustom">
-                 Delight by Macrobyte
+                Delight by Macrobyte
             </p>
             <div class="row mb-0">
-               
+
+            </div>
+        </div>
+    </div>
+    <div class="card card-style text-center d-none" id="noGPS">
+        <div class="content py-5">
+            <h1><i class="fa fa-exclamation-triangle color-red-dark fa-4x"></i></h1>
+            <h1 class="fa-5x pt-5 pb-2">UPS!</h1>
+            <h4 class="text-uppercase pb-3">ACTIVA TU GPS!</h4>
+            <p class="boxed-text-l">
+                No podemos registrar tu ubicacion...
+            </p>
+            <div class="row mb-0">
+
             </div>
         </div>
     </div>
@@ -47,15 +59,16 @@
 @push('scripts')
     <script>
         console.log('hola')
-        $( document ).ready(function() {
+        $(document).ready(function() {
 
-            
+
             navigator.geolocation.getCurrentPosition(function(location) {
                     console.log(location.coords.latitude);
                     console.log(location.coords.longitude);
                     console.log(getDistanceFromLatLonInKm(location.coords.latitude, location.coords.longitude));
                     if (getDistanceFromLatLonInKm(location.coords.latitude, location.coords.longitude) < 0.3) {
-                        console.log(getDistanceFromLatLonInKm(location.coords.latitude, location.coords.longitude))
+                        console.log(getDistanceFromLatLonInKm(location.coords.latitude, location.coords
+                            .longitude))
                         var submit = false;
 
                         setTimeout(function() {
@@ -65,12 +78,13 @@
                             $("#myform").submit(); // if you want            
                         }, 2000);
 
-                        console.log($("#myform"))
+
                     } else {
-                        console.log()
+
                         $('#tooFar').removeClass('d-none');
                         $('#close').addClass('d-none');
-                        $('#textoCustom').html('Estas a '+(getDistanceFromLatLonInKm(location.coords.latitude, location.coords.longitude).toFixed(2)*1000)+' metros de Delight')
+                        $('#textoCustom').html('Estas a ' + (getDistanceFromLatLonInKm(location.coords.latitude,
+                            location.coords.longitude).toFixed(2) * 1000) + ' metros de Delight')
                     }
 
                     function getDistanceFromLatLonInKm(lat2, lon2) {
@@ -98,13 +112,20 @@
                 errores);
 
             function errores(err) {
-                if (err.code == err.TIMEOUT)
+                if (err.code == err.TIMEOUT) {
                     alert("Se ha superado el tiempo de espera");
-                if (err.code == err.PERMISSION_DENIED)
-                    alert('activa tu gps antes de registrar tu entrada')
-                $('#botonRegistro').prop('disabled', true)
-                if (err.code == err.POSITION_UNAVAILABLE)
-                    alert("El dispositivo no pudo recuperar la posición actual");
+                }
+
+                if (err.code == err.PERMISSION_DENIED) {
+                    $('#noGPS').removeClass('d-none');
+                    $('#close').addClass('d-none');
+                }
+
+                if (err.code == err.POSITION_UNAVAILABLE) {
+                    $('#noGPS').removeClass('d-none');
+                    $('#close').addClass('d-none');
+                }
+
             }
         });
     </script>
