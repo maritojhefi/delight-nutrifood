e1e9fa3e8785af2c6f34e082258bde3066bfed68
Mario <maritojhefi@outlook.com>
2022-08-07 22:59:06 -0400

    listo

--- app/Http/Livewire/Admin/WhatsappReporteDesarrolloComponent.php
+++ app/Http/Livewire/Admin/WhatsappReporteDesarrolloComponent.php
@@ -22,8 +22,20 @@ class WhatsappReporteDesarrolloComponent extends Component
         ->leftJoin('users','users.id','plane_user.user_id')
         ->leftJoin('planes','planes.id','plane_user.plane_id')
         ->get();
+
+        $reporteFinalizados = DB::table('plane_user')
+        ->select('plane_user.*','users.name','users.telf','planes.nombre')
+        ->where('plane_user.whatsapp', true)
+        ->where(function ($query) {
+            $query->orWhereDate('start', Carbon::today()->addDay())
+            ->orWhereDate('start', Carbon::today())
+            ->orWhereDate('start', Carbon::today()->subDay());
+        })
+        ->leftJoin('users','users.id','plane_user.user_id')
+        ->leftJoin('planes','planes.id','plane_user.plane_id')
+        ->get();
        // dd($reporte);
-        return view('livewire.admin.whatsapp-reporte-desarrollo-component',compact('reporte'))
+        return view('livewire.admin.whatsapp-reporte-desarrollo-component',compact('reporte','reporteFinalizados'))
         ->extends('admin.master')
         ->section('content');
     }
