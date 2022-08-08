c56f591998f4bc31b167608403aa32b5c236801e
Mario <maritojhefi@outlook.com>
2022-08-07 21:46:36 -0400

    listo

--- app/Http/Livewire/Admin/WhatsappHistorialComponent.php
+++ app/Http/Livewire/Admin/WhatsappHistorialComponent.php
@@ -3,14 +3,16 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\WhatsappHistorial;
 
 class WhatsappHistorialComponent extends Component
 {
+    use WithPagination;
     protected $paginationTheme = 'bootstrap';
     public function render()
     {
-        $whatsapps = WhatsappHistorial::paginate(10);
+        $whatsapps = WhatsappHistorial::orderBy('created_at','desc')->paginate(10);
         return view('livewire.admin.whatsapp-historial-component', compact('whatsapps'))
             ->extends('admin.master')
             ->section('content');;
