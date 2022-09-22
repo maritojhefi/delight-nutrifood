89cc56dba257eb0f6fca373229c58bcffb35e29c
unknown <maritojhefi@gmail.com>
2022-09-22 12:13:00 -0400

    listo

--- resources/views/client/miperfil/menu.blade.php
+++ resources/views/client/miperfil/menu.blade.php
@@ -7,69 +7,88 @@
             <div class="card-center text-center">
                 {{-- <h1 class="fa-3x font-900 mb-0 color-highlight">{{auth()->user()->name}}</h1> --}}
                 <p class="font-13">Eres miembro oficial de<strong> Delight!</strong> </p>
-                <div class="row mx-auto  mb-0" style="max-width:330px;">
-                    <a href="{{route('misplanes')}}" class="col-4">
-                       <i class="fa fa-crown font-50 color-yellow-dark"></i>
-                        <p>Mis Planes</p>
-                    </a>
-                    <a href="{{route('carrito')}}" class="col-4">
+                <div class="row mx-auto  mb-0" >
+                    <div class="card card-style col-3  pt-4 ">
+                        <a href="{{ route('misplanes') }}" class="">
+                            <i class="fa fa-crown font-50 color-yellow-dark"></i>
+                            <p>Mis Planes</p>
+                        </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('carrito') }}" >
                         <i class="fa fa-shopping-cart font-50 color-orange-dark"></i>
-                         <p>Mi Carrito</p>
-                     </a>
-                     <a href="{{route('usuario.saldo')}}" class="col-4">
+                        <p>Mi Carrito</p>
+                    </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('usuario.saldo') }}" >
                         <i class="fa fa-wallet font-50 color-mint-dark"></i>
-                         <p>Mi Saldo</p>
-                     </a>
-                     <a href="{{route('construccion')}}" class="col-4">
+                        <p>Mi Saldo</p>
+                    </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('construccion') }}" >
                         <i class="fa fa-handshake font-50 color-blue-dark"></i>
-                         <p>Asistente Virtual</p>
-                         
-                     </a>
-                     <a href="{{route('construccion')}}" class="col-4">
+                        <p>Asistente Virtual</p>
+
+                    </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('construccion') }}" >
                         <i class="fa fa-video font-50 color-red-dark"></i>
-                         <p>Tutoriales</p>
-                     </a>
-                     <a href="{{route('llenarDatosPerfil')}}" class="col-4">
+                        <p>Tutoriales</p>
+                    </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('llenarDatosPerfil') }}" >
                         <i class="fa fa-user font-50 color-green-dark"></i>
-                         <p>Editar mi perfil</p>
-                     </a>
-                     <a href="{{route('construccion')}}" class="col-4">
+                        <p>Editar mi perfil</p>
+                    </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('construccion') }}" >
                         <i class="fa fa-shopping-bag font-50 color-pink-dark"></i>
-                         <p>Mis Compras</p>
-                     </a>
-                     <a href="{{route('construccion')}}" class="col-4">
+                        <p>Mis Compras</p>
+                    </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('construccion') }}" >
                         <i class="fa fa-newspaper font-50 color-aqua-dark"></i>
-                         <p>Novedades!</p>
-                     </a>
-                     <a href="{{route('construccion')}}" class="col-4">
+                        <p>Novedades!</p>
+                    </a>
+                    </div>
+                    <div class="card card-style col-3  pt-4 ">
+                    <a href="{{ route('construccion') }}" >
                         <i class="fa fa-coins font-50 color-teal-dark"></i>
-                         <p>Mis Puntos</p>
-                     </a>
-                     {{-- <a href="index.html" class="col-4">
+                        <p>Mis Puntos</p>
+                    </a>
+                    </div>
+                    {{-- <a href="index.html" >
                         <i class="fa fa-coins font-50 color-teal-dark"></i>
                          <p>Vista General</p>
                      </a>
-                     <a href="{{route('construccion')}}" class="col-4">
+                     <a href="{{route('construccion')}}" >
                         <i class="fa fa-coins font-50 color-teal-dark"></i>
                          <p>Descuentos!</p>
                      </a> --}}
-                    
+
                 </div>
-                <div class="row mx-auto  mb-0" >
+                <div class="row mx-auto  mb-0">
                     <div class="col-4">
-                        <a href="https://www.facebook.com/DelightNutriFoodEcoTienda" class="icon icon-l color-facebook rounded-xl "><i
-                                class="font-20 fab fa-facebook-f"></i></a>
+                        <a href="https://www.facebook.com/DelightNutriFoodEcoTienda"
+                            class="icon icon-l color-facebook rounded-xl "><i class="font-20 fab fa-facebook-f"></i></a>
                     </div>
                     <div class="col-4">
                         <a href="https://wa.link/ewfjau" class="icon icon-l color-whatsapp rounded-xl"><i
                                 class="font-20 fab fa-whatsapp"></i></a>
                     </div>
                     <div class="col-4">
-                        <a href="https://www.instagram.com/delight_nutrifood_ecotienda/" class="icon icon-l color-instagram rounded-xl "><i
-                                class="font-20 fab fa-instagram"></i></a>
+                        <a href="https://www.instagram.com/delight_nutrifood_ecotienda/"
+                            class="icon icon-l color-instagram rounded-xl "><i class="font-20 fab fa-instagram"></i></a>
                     </div>
                 </div>
-                <p class="opacity-60 font-10">Made with <i class="fa fa-heart"></i> by <span class="copyright-year"><strong>Macrobyte</strong></span> 
+                <p class="opacity-60 font-10">Made with <i class="fa fa-heart"></i> by <span
+                        class="copyright-year"><strong>Macrobyte</strong></span>
                 </p>
             </div>
         </div>
