f424e6fa799c81ae1ee7a29307cddfd16a5d857f
unknown <maritojhefi@gmail.com>
2022-12-06 17:33:45 -0400

    listo

--- app/Helpers/CustomPrint.php
+++ app/Helpers/CustomPrint.php
@@ -2,7 +2,11 @@
 
 namespace App\Helpers;
 
+use Mike42\Escpos\Printer;
+use Illuminate\Support\Str;
+use Mike42\Escpos\EscposImage;
 use Rawilk\Printing\Facades\Printing;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 
 
 
@@ -46,5 +50,69 @@ class CustomPrint {
           return false;
       }
       
+    }
+    public static function imprimirReciboVenta(string $nombreCliente=null,$listaCuenta,$subtotal, $valorSaldo=0,$descuentoProductos=0,$otrosDescuentos=0)
+    {
+      $nombre_impresora = "POS-582";
+            $connector = new WindowsPrintConnector($nombre_impresora);
+            $printer = new Printer($connector);
+            $printer->setJustification(Printer::JUSTIFY_CENTER);
+            $printer->setTextSize(1, 2);
+            $img = EscposImage::load(public_path("delight_logo.jpg"));
+            $printer->bitImageColumnFormat($img);
+            $printer->setTextSize(1, 1);
+            $printer->text("Nutri-Food/Eco-Tienda" . "\n");
+            $printer->feed(1);
+            $printer->text("'NUTRIENDO HABITOS!'" . "\n");
+            $printer->feed(1);
+            $printer->text("Contacto : 78227629" . "\n" . "Campero e/15 de abril y Madrid" . "\n");
+            if (isset($nombreCliente)) {
+                $printer->text("Cliente: " . Str::limit($nombreCliente, '20', '') . "\n");
+            }
+            $printer->setTextSize(2, 2);
+            $printer->text("--------------\n");
+            $printer->setTextSize(1, 1);
+            foreach ($listaCuenta as $list) {
+                $printer->setJustification(Printer::JUSTIFY_LEFT);
+                if ($list['cantidad'] == 1) {
+                    $printer->text($list['cantidad'] . "x " . $list['nombre'] . "\n");
+                } else {
+                    $printer->text($list['cantidad'] . "x " . $list['nombre'] . "(" . $list['precio'] . " c/u)" . "\n");
+                }
+                $printer->setJustification(Printer::JUSTIFY_RIGHT);
+                $printer->text(' Bs ' . floatval($list['subtotal']) . "\n");
+            }
+            $printer->text("--------\n");
+            $printer->setTextSize(1, 1);
+
+            $printer->text("Subtotal: " . floatval($subtotal) . " Bs\n");
+            $printer->text("Descuento por productos: " . floatval($descuentoProductos) . " Bs\n");
+            $printer->text("Otros descuentos: " . floatval($otrosDescuentos) . " Bs\n");
+            if ($valorSaldo != null && $valorSaldo != 0) {
+                $printer->feed(1);
+                $printer->text("Saldo agregado: " . floatval($valorSaldo) . " Bs\n");
+                $printer->feed(1);
+                $printer->setTextSize(1, 2);
+                $printer->text("TOTAL PAGADO: Bs " . $subtotal - $otrosDescuentos - $valorSaldo - $descuentoProductos . "\n");
+                $printer->feed(1);
+            } else {
+                $printer->feed(1);
+                $printer->setTextSize(1, 2);
+                $printer->text("TOTAL PAGADO: Bs " . $subtotal - $otrosDescuentos - $descuentoProductos . "\n");
+                $printer->feed(1);
+            }
+            $printer->setJustification(Printer::JUSTIFY_CENTER);
+            $img = EscposImage::load(public_path("qrcode.png"));
+            $printer->bitImageColumnFormat($img);
+
+            $printer->setTextSize(1, 1);
+            $printer->text("Ingresa a nuestra plataforma!\n");
+            $printer->feed(1);
+            $printer->text("Gracias por tu compra\n");
+            $printer->text("Vuelve pronto!\n");
+            $printer->feed(1);
+            $printer->text(date("Y-m-d H:i:s") . "\n");
+            $printer->feed(3);
+            return $printer;
     }
 }
--- app/Http/Livewire/Admin/Ventas/VentasIndex.php
+++ app/Http/Livewire/Admin/Ventas/VentasIndex.php
@@ -82,23 +82,22 @@ class VentasIndex extends Component
 
             $printer->text("Monto: " . floatval($saldo->monto) . " Bs\n");
             $printer->feed(1);
-            $printer->text($saldo->es_deuda?'A saldo pendiente del cliente':'A favor del cliente'. " \n");
+            $printer->text($saldo->es_deuda ? 'A saldo pendiente del cliente' : 'A favor del cliente' . " \n");
             $printer->feed(1);
-            if($saldo->detalle)
-            {
+            if ($saldo->detalle) {
                 $printer->setTextSize(1, 1);
-                $printer->text("Detalle: " . $saldo->detalle. "\n");
+                $printer->text("Detalle: " . $saldo->detalle . "\n");
             }
-            
+
             $printer->setJustification(Printer::JUSTIFY_CENTER);
             $printer->feed(2);
             $printer->setTextSize(2, 2);
-            $printer->text("TOTAL PAGADO"."\n"." Bs " . $saldo->monto."\n");
+            $printer->text("TOTAL PAGADO" . "\n" . " Bs " . $saldo->monto . "\n");
             $printer->feed(1);
             $printer->setTextSize(1, 1);
 
             $printer->text("--------\n");
-            
+
             $img = EscposImage::load(public_path("qrcode.png"));
             $printer->bitImageColumnFormat($img);
 
@@ -601,11 +600,10 @@ class VentasIndex extends Component
             'type' => 'success',
             'message' => "Se elimino 1 " . $producto->nombre . " de esta venta"
         ]);
-        if($producto->subcategoria->categoria->nombre!='ECO-TIENDA')//revisa si es de cocina/panaderia el producto para que actualice en la vista de cocina
+        if ($producto->subcategoria->categoria->nombre != 'ECO-TIENDA') //revisa si es de cocina/panaderia el producto para que actualice en la vista de cocina
         {
-            event(new CocinaPedidoEvent("Se actualizo la mesa ".$this->cuenta->id));
+            event(new CocinaPedidoEvent("Se actualizo la mesa " . $this->cuenta->id));
         }
-        
     }
 
     public function eliminarproducto(Producto $producto)
@@ -622,9 +620,9 @@ class VentasIndex extends Component
             'type' => 'success',
             'message' => "Se elimino a " . $producto->nombre . " de esta venta"
         ]);
-        if($producto->subcategoria->categoria->nombre!='ECO-TIENDA')//revisa si es de cocina/panaderia el producto para que actualice en la vista de cocina
+        if ($producto->subcategoria->categoria->nombre != 'ECO-TIENDA') //revisa si es de cocina/panaderia el producto para que actualice en la vista de cocina
         {
-            event(new CocinaPedidoEvent("Se actualizo la mesa ".$this->cuenta->id));
+            event(new CocinaPedidoEvent("Se actualizo la mesa " . $this->cuenta->id));
         }
     }
 
@@ -641,7 +639,7 @@ class VentasIndex extends Component
 
     public function eliminar(Venta $venta)
     {
-        
+
         $venta->delete();
         $this->dispatchBrowserEvent('alert', [
             'type' => 'warning',
@@ -652,7 +650,6 @@ class VentasIndex extends Component
                 $this->reset();
             }
         }
-        
     }
     public function agregardesdeplan($user, $plan, $producto)
     {
@@ -759,71 +756,18 @@ class VentasIndex extends Component
     public function imprimir()
     {
 
-        QrCode::format('png')->size(150)->generate('https://delight-nutrifood.com/miperfil', public_path() . '/qrcode.png');
+        //QrCode::format('png')->size(150)->generate('https://delight-nutrifood.com/miperfil', public_path() . '/qrcode.png');
         if ($this->cuenta->sucursale->id_impresora) {
 
-            $nombre_impresora = "POS-582";
-            $connector = new WindowsPrintConnector($nombre_impresora);
-            $printer = new Printer($connector);
-            $printer->setJustification(Printer::JUSTIFY_CENTER);
-            $printer->setTextSize(1, 2);
-            //$printer->text("DELIGHT" . "\n");
-            $img = EscposImage::load(public_path("delight_logo.jpg"));
-            $printer->bitImageColumnFormat($img);
-            $printer->setTextSize(1, 1);
-            $printer->text("Nutri-Food/Eco-Tienda" . "\n");
-            $printer->feed(1);
-            $printer->text("'NUTRIENDO HABITOS!'" . "\n");
-            $printer->feed(1);
-            $printer->text("Contacto : 78227629" . "\n" . "Campero e/15 de abril y Madrid" . "\n");
-            if (isset($this->cuenta->cliente->name)) {
-                $printer->text("Cliente: " . Str::limit($this->cuenta->cliente->name, '20', '') . "\n");
-            }
-            $printer->setTextSize(2, 2);
-            $printer->text("--------------\n");
-            $printer->setTextSize(1, 1);
-            foreach ($this->listacuenta as $list) {
-                $printer->setJustification(Printer::JUSTIFY_LEFT);
-                if ($list['cantidad'] == 1) {
-                    $printer->text($list['cantidad'] . "x " . $list['nombre'] . "\n");
-                } else {
-                    $printer->text($list['cantidad'] . "x " . $list['nombre'] . "(" . $list['precio'] . " c/u)" . "\n");
-                }
-                $printer->setJustification(Printer::JUSTIFY_RIGHT);
-                $printer->text(' Bs ' . floatval($list['subtotal']) . "\n");
-            }
-            $printer->text("--------\n");
-            $printer->setTextSize(1, 1);
-
-            $printer->text("Subtotal: " . floatval($this->subtotal) . " Bs\n");
-            $printer->text("Descuento por productos: " . floatval($this->descuentoProductos) . " Bs\n");
-            $printer->text("Otros descuentos: " . floatval($this->cuenta->descuento) . " Bs\n");
-            if ($this->valorSaldo != null && $this->valorSaldo != 0) {
-                $printer->feed(1);
-                $printer->text("Saldo agregado: " . floatval($this->valorSaldo) . " Bs\n");
-                $printer->feed(1);
-                $printer->setTextSize(1, 2);
-                $printer->text("TOTAL PAGADO: Bs " . $this->subtotal - $this->cuenta->descuento - $this->valorSaldo - $this->descuentoProductos . "\n");
-                $printer->feed(1);
-            } else {
-                $printer->feed(1);
-                $printer->setTextSize(1, 2);
-                $printer->text("TOTAL PAGADO: Bs " . $this->subtotal - $this->cuenta->descuento - $this->descuentoProductos . "\n");
-                $printer->feed(1);
-            }
-            $printer->setJustification(Printer::JUSTIFY_CENTER);
-            $img = EscposImage::load(public_path("qrcode.png"));
-            $printer->bitImageColumnFormat($img);
-
-            $printer->setTextSize(1, 1);
-            $printer->text("Ingresa a nuestra plataforma!\n");
-            $printer->feed(1);
-            $printer->text("Gracias por tu compra\n");
-            $printer->text("Vuelve pronto!\n");
-            $printer->feed(1);
-            $printer->text(date("Y-m-d H:i:s") . "\n");
-            $printer->feed(3);
-            $respuesta = CustomPrint::imprimir($printer, $this->cuenta->sucursale->id_impresora);
+            $recibo = CustomPrint::imprimirReciboVenta(
+                isset($this->cliente->name) ? $this->cliente->name : null,
+                $this->listacuenta,
+                $this->subtotal,
+                isset($this->valorSaldo) ? $this->valorSaldo : 0,
+                $this->descuentoProductos,
+                $this->descuento
+            );
+            $respuesta = CustomPrint::imprimir($recibo, $this->cuenta->sucursale->id_impresora);
             if ($respuesta == true) {
                 $this->dispatchBrowserEvent('alert', [
                     'type' => 'success',
