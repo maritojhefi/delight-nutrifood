6e3e3035b33b6e2efb51eef7ae05f25ebe6624fe
Mario <maritojhefi@outlook.com>
2023-09-22 05:26:32 -0400

    pruebas

--- resources/views/pdf/recibo-nuevo.blade.php
+++ resources/views/pdf/recibo-nuevo.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Receipt Sample</title>
+    <title>Recibo de venta</title>
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
+        /* @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap'); */
 
         * {
             margin: 0;
