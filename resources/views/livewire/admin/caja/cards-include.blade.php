80f742c67e05393d4395f89c98c66cc608dbfed6
unknown <maritojhefi@gmail.com>
2023-09-28 16:09:10 -0400

    listo

--- resources/views/livewire/admin/caja/cards-include.blade.php
+++ resources/views/livewire/admin/caja/cards-include.blade.php
@@ -329,7 +329,7 @@
                                             class="mb-0">{{ $ventasHoy->where('tipo', 'tarjeta')->sum('total') - $ventasHoy->where('tipo', 'tarjeta')->sum('saldo') - $ventasHoy->where('tipo', 'tarjeta')->sum('descuento') }}
                                             Bs</span>
                                     </li>
-                                    {{-- <li class="list-group-item d-flex px-0 justify-content-between">
+                                    <li class="list-group-item d-flex px-0 justify-content-between">
                                         <strong>Banco Bisa</strong>
                                         <span
                                             class="mb-0">{{ $ventasHoy->where('tipo', 'banco-bisa')->sum('total') - $ventasHoy->where('tipo', 'banco-bisa')->sum('saldo') - $ventasHoy->where('tipo', 'banco-bisa')->sum('descuento') }}
@@ -346,7 +346,7 @@
                                         <span
                                             class="mb-0">{{ $ventasHoy->where('tipo', 'banco-sol')->sum('total') - $ventasHoy->where('tipo', 'banco-sol')->sum('saldo') - $ventasHoy->where('tipo', 'banco-sol')->sum('descuento') }}
                                             Bs</span>
-                                    </li> --}}
+                                    </li>
                                     <li class="list-group-item d-flex px-0 justify-content-between">
                                         <strong>Banco BNB</strong>
                                         <span
--- resources/views/livewire/admin/caja/reportes.blade.php
+++ resources/views/livewire/admin/caja/reportes.blade.php
@@ -496,7 +496,7 @@
                                             class="mb-0">{{ $saldosHoy->where('tipo', 'tarjeta')->sum('monto') + $ventasHoy->where('tipo', 'tarjeta')->sum('total') - $ventasHoy->where('tipo', 'tarjeta')->sum('saldo') - $ventasHoy->where('tipo', 'tarjeta')->sum('descuento') }}
                                             Bs</span>
                                     </li>
-                                    {{-- <li class="list-group-item d-flex px-0 justify-content-between">
+                                    <li class="list-group-item d-flex px-0 justify-content-between">
                                         <strong>Banco Bisa</strong>
                                         <span
                                             class="mb-0">{{ $saldosHoy->where('tipo', 'banco-bisa')->sum('monto') + $ventasHoy->where('tipo', 'banco-bisa')->sum('total') - $ventasHoy->where('tipo', 'banco-bisa')->sum('saldo') - $ventasHoy->where('tipo', 'banco-bisa')->sum('descuento') }}
@@ -513,7 +513,7 @@
                                         <span
                                             class="mb-0">{{ $saldosHoy->where('tipo', 'banco-sol')->sum('monto') + $ventasHoy->where('tipo', 'banco-sol')->sum('total') - $ventasHoy->where('tipo', 'banco-sol')->sum('saldo') - $ventasHoy->where('tipo', 'banco-sol')->sum('descuento') }}
                                             Bs</span>
-                                    </li> --}}
+                                    </li>
 
                                     <li class="list-group-item d-flex px-0 justify-content-between">
                                         <strong>Banco BNB</strong>
