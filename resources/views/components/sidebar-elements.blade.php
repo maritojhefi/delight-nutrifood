54210dfbfc9c207e37eccae505efcce9425567a9
Mario <maritojhefi@outlook.com>
2024-10-31 19:07:53 -0400

    mejoras vista cocina

--- resources/views/components/sidebar-elements.blade.php
+++ resources/views/components/sidebar-elements.blade.php
@@ -1,13 +1,13 @@
 
 
-<li>
-    <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+<li class="p-0 m-0 ps-2">
+    <a class="has-arrow ai-icon py-3" href="javascript:void()" aria-expanded="false">
     {{$slot}}
         <span class="nav-text">{{$titulo}}</span>
     </a>
     <ul aria-expanded="false" class="mm-collapse">
         @foreach ($lista as $subtitulo=>$ruta)
-        <li ><a href="{{route($ruta)}}">{{$subtitulo}}</a></li>
+        <li class="p-0 m-0 ps-2"><a class="py-2" href="{{route($ruta)}}">{{$subtitulo}}</a></li>
         @endforeach
         
     </ul>
