cd0d81a6383cb3e7af8f514de0ed4f0d9def2efa
maritojhefi <maritojhefi@gmail.com>
2025-05-30 13:41:26 -0400

    fixreadi

--- app/Http/Livewire/Admin/Ventas/VentasIndex.php
+++ app/Http/Livewire/Admin/Ventas/VentasIndex.php
@@ -1003,7 +1003,7 @@ class VentasIndex extends Component
                         $saldoAFavorCliente = true;
                         $montoSaldo = $this->totalAcumuladoMetodos - $this->subtotalConDescuento;
                     }
-                    dd($this->subtotalConDescuento);
+                    // dd($this->subtotalConDescuento);
                     $cuentaguardada = Historial_venta::create([
                         'caja_id' => $cajaactiva->id,
                         'usuario_id' => auth()->user()->id,
