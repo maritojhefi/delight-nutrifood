689f71eb551b23a0db82f6409aa3d1a8bf78df8d
Alexander-RG <alexander.rgareca@gmail.com>
2025-07-16 18:28:19 -0400

    modificaci'on inicial ui formulario login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,59 @@
 @extends('client.masterSinFooter')
 @section('content')
     <x-cabecera-pagina titulo="Inicia Sesion" cabecera="bordeado" />
-
-    <div class="card card-style bg-24" style="height: 573px;">
-        <div class="card-center">
-            <div class="ms-5 me-5">
-                <img src="{{ asset(GlobalHelper::getValorAtributoSetting('logo_small')) }}" class="img mx-auto d-block " style="width:100px" alt="">
-                <p class="color-highlight font-12 text-center ">Bienvenido a {{ GlobalHelper::getValorAtributoSetting('nombre_sistema') }}!</p>
-                <div class="mt-2 mb-0">
-                    <form action="{{ route('login') }}" method="post">
+    <div class="d-flex justify-content-center">
+        <div class="card card-style login-card bg-24" style="height: 500px; width: 350px;">
+            <div class="card-center">
+                <div class="px-5 align-content-center ">
+                    <div class="d-flex flex-column">
+                        <img src="{{ asset(GlobalHelper::getValorAtributoSetting('logo_small')) }}" class="img mx-auto d-block" style="width:100px" alt="">
+                        <p class="mt-2 color-highlight font-26 font-weight-bold text-center ">Bienvenido a {{ GlobalHelper::getValorAtributoSetting('nombre_sistema') }}!</p>
+                    </div>
+                    
+                    <form action="{{ route('login') }}" method="post" class="d-flex flex-column justify-content-center">
                         @csrf
-                        <div class="input-style input-transparent no-borders has-icon validate-field">
-                            <i class="fa fa-user"></i>
-                            <input type="email" class="form-control validate-name" id="form1a" placeholder="Email"
-                                name="email" value="{{ old('email') }}" required>
-                            <label for="form1a" class="color-blue-dark font-10 mt-1">Email</label>
+                        <label for="form1a">Correo electronico</label>
 
+                        <div class="input-style has-icon validate-field d-flex flex-row align-content-center">
+                                <i class="fa fa-user ms-2 input-icon"></i>
+                                <input type="email" class="form-control rounded-sm validate-email font-13" id="form1a"
+                                    name="email" value="{{ old('email') }}" required>
                         </div>
-                        @error('email')
-                            <small class="text-danger">{{ $message }}</small>
-                        @enderror
-                        <div class="input-style input-transparent no-borders has-icon validate-field mt-4">
-                            <i class="fa fa-lock"></i>
-                            <input type="password" class="form-control validate-password" id="form3a"
-                                placeholder="Contraseña" required name="password">
-                            <label for="form3a" class="color-blue-dark font-10 mt-1">Contraseña</label>
 
+                        @error('email')
+                            <small class="text-danger">{{ $message }}</ small>
+                        @enderror
+
+                        <label for="form3a">Contraseña</label>
+                        {{-- <div class="input-style no-borders has-icon validate-field d-flex flex-row align-content-center"> --}}
+                        <div class="input-style no-borders has-icon validate-field d-flex flex-row align-content-center"> 
+                            <i class="fa fa-lock ms-2 input-icon"></i>
+                            <input type="password" class="form-control rounded-sm validate-password font-13" id="form3a"
+                                required name="password">
                         </div>
                         @error('password')
                             <small class="text-danger">{{ $message }}</small>
                         @enderror
-                        <button type="submit"
-                            class="btn btn-m mt-2 mb-4 btn-full btn-block bg-green-dark rounded-sm text-uppercase font-900 loader">Iniciar
-                            Sesion</button>
-
-
-                        <div class="row">
-                            <div class="col-12 pt-3 text-end"><a href="{{ route('register') }}"
-                                    class="color-white opacity-50 font-15">Aun sin cuenta? <strong>Registrate aqui</strong>
-                                </a></div>
+                        <div class="d-flex justify-content-center"> 
+                            <button type="submit"
+                                class="btn btn-m mt-2 mb-4 bg-green-dark rounded-sm text-uppercase font-900 loader"
+                                style="width: 160px;"
+                                >
+                                Iniciar Sesion
+                            </button>
                         </div>
                     </form>
                 </div>
             </div>
+            <div class="card-bottom">
+                <div class="row">
+                    <div class="text-end pe-5">
+                        <a href="{{ route('register') }}"
+                            class="color-black opacity-50 font-15">Aun sin cuenta? <strong>Registrate aqui</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-overlay bg-black opacity-85"></div>
     </div>
 @endsection
--- resources/views/client/master.blade.php
+++ resources/views/client/master.blade.php
@@ -12,7 +12,6 @@
 
     <link rel="stylesheet" type="text/css" href="{{ asset('styles/bootstrap.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('styles/custom.css') }}">
-
     <link
         href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900|Roboto:300,300i,400,400i,500,500i,700,700i,900,900i&amp;display=swap"
         rel="stylesheet">
