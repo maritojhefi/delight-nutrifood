6a441fef0e1d55b282b7f145da5722e32fc59c5f
Alexander-RG <alexander.rgareca@gmail.com>
2025-11-04 16:05:41 -0400

    cambio inicio sesión de correo a número telefónico

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,32 +2,43 @@
 @section('content')
     <x-cabecera-pagina titulo="Inicia Sesion" cabecera="appkit" />
     <div class="d-flex justify-content-center">
-        <div class="card card-style login-card bg-24" style="height: 500px; width: 380px;">
-            <div class="card-center">
-                <div class="px-5">
+        <div class="card card-style login-card bg-24" style="height: 550px; width: 380px;">
+            <div class="card-center mt-n3">
+                <div class="px-4">
                     <div class="d-flex flex-column mb-3">
                         <img src="{{ asset(GlobalHelper::getValorAtributoSetting('logo_small')) }}" class="img mx-auto d-block" style="width:100px" alt="">
                         <h3 class="mt-2 font-26 text-center ">¡Bienvenido a {{ GlobalHelper::getValorAtributoSetting('nombre_sistema') }}!</h3>
                     </div>
-                    <form action="{{ route('login') }}" method="post" class="d-flex flex-column justify-content-center mx-2">
+                    <form action="{{ route('login') }}" method="post" class="d-flex flex-column justify-content-center gap-1">
                         @csrf
-                        <label for="form1a">Correo electronico</label>
-                        <div class="input-style has-icon validate-field d-flex flex-row align-content-center">
-                            <i class="fa fa-user ms-2 input-icon"></i>
-                            <input type="email" class="form-control rounded-sm validate-email font-13" id="form1a" name="email" value="{{ old('email') }}" required>
-                        </div>
+                        <label for="form1a">Número de Teléfono</label>
+                    
 
-                        @error('email')
-                            <small class="text-danger">{{ $message }}</ small>
+                        <div class="d-flex flex-row gap-2">
+                            <x-countrycode-select></x-countrycode-select>
+                            <div class="input-with-icon-container mb-0 validate-field d-flex flex-grow-1">
+                                <i class="fa fa-phone position-absolute ms-2 align-self-center"></i>
+
+                                <input class="form-control rounded-sm validate-email font-13" id="form1a" name="telf" value="{{ old('telf') }}">
+                            </div>
+                        </div>
+                        
+
+                        @error('telf')
+                            <small class="text-danger">{{ $message }}</small>
                         @enderror
                         
                         <label for="password-input">Contraseña</label>
-                        <div class="input-style no-borders has-icon validate-field d-flex flex-row align-content-center position-relative">
-                            <i class="fa fa-key ms-2 input-icon"></i>
-                            <input id="password-input" type="password" class="form-control rounded-sm validate-password font-13" required name="password">
-                            <button type="button" class="me-2 btn btn-link position-absolute end-0 top-50 translate-middle-y pe-3 password-toggle" style="border: none; background: none; z-index: 10;">
+                        <!-- <div class="input-style no-borders has-icon validate-field d-flex flex-row align-content-center position-relative"> -->
+                        <div class="input-with-icon-container mb-0 validate-field d-flex flex-grow-1">
+                            <i class="fa fa-key position-absolute ms-2 align-self-center"></i>
+                            <!-- <i class="fa fa-key ms-2 input-icon"></i> -->
+                            <input id="password-input" type="password" class="form-control rounded-sm validate-password font-13" name="password">
+                            <i class="fa fa-lock password-toggle position-absolute me-4 end-0 align-self-center" id="toggleIcon"></i>
+
+                            <!-- <button type="button" class="me-2 btn btn-link position-absolute end-0 password-toggle" style="border: none; background: none; z-index: 10;">
                                 <i class="fa fa-lock" id="toggleIcon"></i>
-                            </button>
+                            </button> -->
                         </div>
                         @error('password')
                             <small class="text-danger">{{ $message }}</small>
@@ -60,6 +71,7 @@
         const passwordInput = document.getElementById('password-input');
         const toggleButton = document.querySelector('.password-toggle');
         const toggleIcon = document.getElementById('toggleIcon');
+        // reinitializeLucideIcons();
 
         if (passwordInput && toggleButton && toggleIcon) {
             toggleButton.addEventListener('click', function() {
