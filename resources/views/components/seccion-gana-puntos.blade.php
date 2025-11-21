cbeb8a60f345b01e62bb7c987d38fe48a3e34d56
Alexander-RG <alexander.rgareca@gmail.com>
2025-11-21 18:36:14 -0400

    mejora seccion gana puntos y componente reutilizable card producto

--- resources/views/components/seccion-gana-puntos.blade.php
+++ resources/views/components/seccion-gana-puntos.blade.php
@@ -1,3 +1,4 @@
+@push('content-comentado')
 <div class="card card-style rounded-md mx-0 preload-img mt-2 entered loaded" data-src="images/pictures/20s.jpg" data-ll-status="loaded"
     style="background-image: url({{ asset('imagenes/delight/default-bg-vertical.jpg') }});">
     <div class="card-body">
@@ -38,4 +39,60 @@
     </div>
     <div class="card-overlay bg-highlight opacity-90"></div>
     <div class="card-overlay dark-mode-tint"></div>
+</div>
+@endpush
+
+
+<div class="card card-style">
+    <div class="content">
+        <p class="mb-n1 color-highlight font-600">
+            {{ GlobalHelper::getValorAtributoSetting('nombre_sistema') . ' - ' . GlobalHelper::getValorAtributoSetting('slogan') }}
+        </p>
+        <h2 class="font-24 font-800">
+            Productos puntuados
+        </h2>
+        <p class="my-3">
+            ¡Adquiere puntos comprando los siguientes productos!
+        </p>
+        <div class="row mb-0">
+            @foreach ($productos as $producto)
+            <div class="col-6 ">
+                <!-- <div class="card rounded-m shadow-l"
+                    data-card-height="200"
+                    style="background-image: url('{{ $producto->pathAttachment() }}');">
+                    >
+                    <div class="card-bottom text-center">
+                        <h2 class="color-white font-700 font-18 mb-3 px-1">{{ $producto->nombre }}</h2>
+                        <a class="btn btn-xs rounded-m btn-full font-13 font-600 gradient-blue rounded-s me-2 ms-2 mb-2"
+                            href="{{ route('delight.detalleproducto', $producto->id) }}">
+                            {{ $producto->puntos }} Puntos
+                        </a>
+                    </div>
+                    <div class="card-overlay bg-gradient dark-mode-tint"></div>
+                </div> -->
+                <!-- <div class="card card-style mx-0 bg-dtheme-blue">
+                    <div class="d-flex text-center align-items-end"
+                        style="
+                            background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)), url('{{ $producto->pathAttachment() }}');
+                            height: 8rem;
+                            background-size: cover;
+                            background-position: center;
+                        ">
+                        <h2 class="color-white font-700 font-18 mb-3 px-1">{{ $producto->nombre }}</h2>
+                    </div>
+                    <div class="px-2">
+                        <div class="d-flex flex-row align-items-center justify-content-between my-2">
+                            <h5 class="font-14 mb-0">Bs. {{ $producto->precioReal() }}</h5>
+                            <span class=" badge badge-xl gradient-blue">
+                                {{ $producto->puntos }} Puntos</span>
+                        </div>
+                        <a href="#" class="btn btn-xxs btn-full border-highlight rounded-s color-highlight mb-3">Order</a>
+                    </div>
+                </div> -->
+                <x-card-producto-individual :producto="$producto" />
+            </div>
+            @endforeach
+        </div>
+    </div>
+    
 </div>
